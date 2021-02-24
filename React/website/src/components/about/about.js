250a51411bc5c51d8a489f0bdb1599d319553af1
Will Sather <sathe151@umn.edu>
2021-02-24 13:43:20 -0600

    React: About CSS Table Flex

--- React/website/src/components/about/about.js
+++ React/website/src/components/about/about.js
@@ -12,23 +12,29 @@ function About() {
                 hi, i'm will :)
             </div>
 
-            <div class="picture">
-                <img src={picture} alt="Will Sather" width="600" height="auto"/>
-            </div>
+            <div class="flex">
 
-            <div className="about-paragraph">
-                i am a junior at university of minnesota studying computer science.
-                i built this website as a learning day project so i could teach myself 
-                react and further my html/front-end development skills.  this is hosted on my domain 
-                through a subdomain on netlify
+                <div className="about-paragraph">
+                    i am a junior at university of minnesota studying computer science.
+                    i built this website as a learning day project so i could teach myself 
+                    react and further my html/front-end development skills.  this is hosted on my domain 
+                    through a subdomain on netlify.
 
-                <br></br>
-                <br></br>
-                <br></br>
-                outside of the experience listed, i have some previous work with html, react,
-                and javascript through the personal websites i have made over the years.  
-                professionally, i have never worked on front-end development; however, i have
-                found the work quite fascinating just on my own time.  
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    
+                    outside of the experience listed, i have some previous work with html, react,
+                    and javascript through the personal websites i have made over the years.  
+                    professionally, i have never worked on front-end development; however, i have
+                    found the work quite fascinating just on my own time.  
+                
+                </div>
+
+                <div class="picture">
+                    <img src={picture} alt="Will Sather" width="600px"/>
+                </div>
+
             </div>
         </div>
     );
