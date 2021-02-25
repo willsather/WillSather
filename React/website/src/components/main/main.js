5dcf748718dd06e251fe25088b45ff451b50af68
Will Sather <sathe151@umn.edu>
2021-02-25 16:57:33 -0600

    React: Deploy on Main

--- React/website/src/components/main/main.js
+++ React/website/src/components/main/main.js
@@ -10,7 +10,7 @@ function Main() {
             <h2 className="main-body">
                 developed with react.
             </h2>
-            
+
             <h3 className="main-body">
                 deployed on netlify. 
             </h3>
@@ -21,6 +21,7 @@ function Main() {
                     <li><a href="https://www.facebook.com/will.sather.92" Style="color:black" target="_blank">Facebook</a></li>
                     <li><a href="https://github.com/willsather/Will-Sather" Style="color:black" target="_blank">Github</a></li>
                     <li><a href="https://twitter.com/will_sather" Style="color:black" target="_blank">Twitter</a></li>
+                    <li><a href="https://www.linkedin.com/in/willsather" Style="color:black" target="_blank">Linkedin</a></li>
                     <li><a href="https://www.linkedin.com/in/willsather" Style="color:black" target="_blank">Linkedin</a></li>
                 </ul>
             </div>
