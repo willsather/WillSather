c7d104d78cdb26a03fb316eb4507c3c96bcfaa15
WillSather <will@sather.ws>
2022-11-16 21:37:51 -0500

    Update web dependencies

--- angular-website/website/src/app/app.component.spec.ts
+++ angular-website/website/src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'website'`, () => {
+  it(`should have as title 'Will Sather'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('website');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('website app is running!');
+    expect(app.title).toEqual('Will Sather');
   });
 });
