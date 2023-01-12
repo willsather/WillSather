01c340ec9f6e039d182166af6cd12a5e2cfe4a09
Will Sather <will@sather.ws>
2023-01-12 14:24:53 -0500

    Move project files into individual repos

--- angular-website/website/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-export class AppRoutes {
-    static readonly HomeBranch = "";
-    static readonly ExperienceBranch = "experience";
-    static readonly AboutBranch = "about";
-
-    static readonly ExperiencePath = "/" + AppRoutes.ExperienceBranch;
-    static readonly AboutPath = "/" + AppRoutes.AboutBranch;
-}
--- angular-website/website/src/app/components/experience/experience.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'experience-component',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
-})
-export class ExperienceComponent {
-}
